70201e12369bd5e63a35b4a6dd0d932ec7c403b3
Louis Lhotte <louis.lhotte@student-cs.fr>
2025-01-25 17:47:01 +0100

    Comments

--- LogoLho/main.py
+++ LogoLho/main.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 
 ds = load_dataset("logo-wizard/modern-logo-dataset")
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32") #On peut mettre d'autres modèles
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.ToTensor(),
@@ -26,7 +26,7 @@ model = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-3")
 config = LoraConfig(
     r=4,
     lora_alpha=16,
-    target_modules=["attn1", "attn2"],
+    target_modules=["attn1", "attn2"], #potentiellement à changer en fonction des layers target qu'on veut fine-tuner
     lora_dropout=0.05,
     bias="none"
 )
